72a23a3240d6ab8e2c5b1ffc21f59988495da984
ajhamwood <ajh@tuta.io>
2017-05-03 13:05:16 +1000

    Converted Speakie to "_" private properties.

--- speakie.js
+++ speakie.js
@@ -8,14 +8,14 @@ var Speakie = (function () {
   function hear (utterList) {
     var utterance, syntax, result, params;
     this.emit("hear", utterList);
-    p.debug && console.log("Heard:");
+    (_ = this._).debug && console.log("Heard:");
     for (var utterance of utterList) {
       utterance = utterance.trim();
-      p.debug && console.log("    " + utterance);
-      for (var [command, syntax] of p.syntaxes) {
+      _.debug && console.log("    " + utterance);
+      for (var [command, syntax] of _.syntaxes) {
         if (result = syntax.interpretation.exec(utterance)) {
           params = result.slice(1);
-          p.debug && console.log("Understood: " + command.text + (params.length ? " , with data: " + params : ""));
+          _.debug && console.log("Understood: " + command.text + (params.length ? " , with data: " + params : ""));
           this.emit("hear-words", utterance, command.text, utterList);
           return syntax.fns.forEach(fn => fn.call(this, params));
         }
@@ -47,8 +47,8 @@ var Speakie = (function () {
     if (!Recog) return null;
     options = Object.assign({lang: "en", debug: false, continuous: false}, options);
 
-    // Private properties
-    p = {
+    // "Private" properties
+    var _ = this._ = {
       events: {},
       syntaxes: new Map(),
       debug: options.debug,
@@ -66,20 +66,20 @@ var Speakie = (function () {
         onspeechend: () => this.emit("speechend"),
         onerror: event => {
           this.emit("error", event);
-          p.active = false;
+          _.active = false;
           switch (event.error) {
             case "network": this.emit("error-network", event) ;break;
             case "not-allowed":
             case "service-not-allowed":
-            p.autoRestart = false;
-            this.emit("error-permission-" + new Date().getTime() - lastStartedAt < 200 ? "blocked" : "denied", event)
+            _.autoRestart = false;
+            this.emit("error-permission-" + new Date().getTime() - _.lastStartedAt < 200 ? "blocked" : "denied", event)
           }
         },
         onend: () => {
           this.emit("end");
-          if (p.autoRestart) {
-            let t = new Date().getTime() - p.lastStartedAt;
-            t < 1000 ? setTimeout(() => p.autoRestart || this.start(), 1000 - t) : this.start();
+          if (_.autoRestart) {
+            let t = new Date().getTime() - _.lastStartedAt;
+            t < 1000 ? setTimeout(() => _.autoRestart || this.start(), 1000 - t) : this.start();
           }
         },
         onresult: event => {
@@ -87,63 +87,46 @@ var Speakie = (function () {
         },
         onnomatch: () => this.emit("hear-no-words")
       }),
-      voice: null,
-      i18n: {},
-      i18nword: {}
+      voice: null
     };
 
 
     // Speech synthesis
     // - Used like so: voicie = new speakie.Voicie(); voicie.say("hey")
 
-    function Voicie () { p.voice = window.speechSynthesis.getVoices().find(a => a.lang.indexOf(p.recog.lang) == 0) }
+    function Voicie () { self._.voice = window.speechSynthesis.getVoices().find(a => a.lang.indexOf(self._.recog.lang) == 0) }
     Voicie.prototype = {
       say: function (text) {
         if (text.constructor.name == "I18nWord") text = text.text;
         var Utterance = ["", "webkit", "moz", "ms", "o"].reduce((a, v) => a || window[v + "SpeechSynthesisUtterance"], 0),
             utterance = new Utterance(text);
-        Object.assign(utterance, { voice: p.voice, pitch: 1, rate: 1 });
+        Object.assign(utterance, { voice: self._.voice, pitch: 1, rate: 1 });
         window.speechSynthesis.speak(utterance)
       },
-      constructor: Voicie
+      constructor: (this.Voicie = Voicie)
     };
-    this.Voicie = new Proxy(Voicie, { construct (con, args) {
-      p = priv.get(self); var obj = new con(...args);
-      return new Proxy(obj, { get (obj, key) {
-        p = priv.get(self); return obj[key]
-      }})
-    }});
 
 
     // Internationalisation
     // - Constructed with one parameter of the form: {word: {en: "Word", "ja-JP": "言葉" ...} ...}
     // - Used like so: var i18n = new speakie.I18n({...}); i18n.word // "Word"
 
-    function I18n (o) {
-      p.i18n = o;
-      for (let word in o) p.i18n[word] = new self.I18nWord(p.i18n[word])
-    }
-    I18n.prototype.constructor = I18n;
-    this.I18n = new Proxy(I18n, { construct (con, args) {
-      p = priv.get(self); var obj = new con(...args); priv.set(obj, p.i18n);
-      return new Proxy(obj, { get (obj, key) {
-        p = priv.get(self); p.i18n = priv.get(obj); return p.i18n[key]
-      }})
-    }});
+    this.I18n = (function () {
+      function I18n (o) { for (let word in o) this[word] = new self.I18nWord(o[word]) }
+      I18n.prototype.constructor = I18n;
+      return I18n
+    })();
 
     // - Constructed with one parameter of the form: {en: "Word", "ja-JP": "言葉" ...}
-    function I18nWord (o) { p.i18nword = o }
-    I18nWord.prototype = {
-      get text() { return Object.entries(p.i18nword).find(e => e[0].indexOf(p.recog.lang) >= 0)[1] },
-      set text(_) { return false },
-      constructor: I18nWord
-    };
-    this.I18nWord = new Proxy(I18nWord, { construct (con, args) {
-      p = priv.get(self); var obj = new con(...args); priv.set(obj, p.i18nword);
-      return new Proxy(obj, { get (obj, key) {
-         p = priv.get(self); p.i18nword = priv.get(obj); return obj[key]
-      }})
-    }})
+    this.I18nWord = (function () {
+      function I18nWord (o) { for (let lang in o) this[lang] = o[lang] }
+      I18nWord.prototype = {
+        get text() { return Object.entries(this).find(e => e[0].indexOf(self._.recog.lang) >= 0)[1] },
+        set text(v) { return false },
+        constructor: I18nWord
+      };
+      return I18nWord
+    })()
 
   }
 
@@ -151,58 +134,56 @@ var Speakie = (function () {
   Speakie.prototype = {
     // Listen for events
     // - Use a named function at .on() to remove with .stop()
-    on: function (event, fn) { (p.events[event] = p.events[event] || []).push(fn) },
-    emit: function (event, ...args) { p.events[event] && p.events[event].forEach(fn => fn.apply(this, args)) },
-    stop: function (event, fname = "") { p.events[event].splice(p.events[event].findIndex(fn => fn.name == fname), 1) },
+    on: function (event, fn) { (this._.events[event] = this._.events[event] || []).push(fn) },
+    emit: function (event, ...args) { this._.events[event] && this._.events[event].forEach(fn => fn.apply(this, args)) },
+    stop: function (event, fname = "") { this._.events[event].splice(this._.events[event].findIndex(fn => fn.name == fname), 1) },
 
+    // Turning Speakie on and off
     start: function () {
-      p.lastStartedAt = new Date().getTime();
-      p.autoRestart = true;
-      p.active = true;
-      p.recog.start()
+      (_ = this._).lastStartedAt = new Date().getTime();
+      _.autoRestart = true;
+      _.active = true;
+      _.recog.start()
     },
     abort: function () {
-      p.autoRestart = false;
-      p.active = false;
+      (_ = this._).autoRestart = false;
+      _.active = false;
       this.emit("end");
-      p.recog.abort()
+      _.recog.abort()
     },
 
     // Set the language for all Speakie objects
-    get lang () { return p.recog.lang },
+    get lang () { return this._.recog.lang },
     set lang (val) {
-      if (p) {
-        p.recog.lang = val;
-        p.recog.stop();
-        p.syntaxes = new Map([...p.syntaxes].map(([command, syntax]) => {
-          command.lang = val;
-          syntax.interpretation = command.text.constructor.name === "RegExp" ? command.text : know(command.text);
-          return [command, syntax]
-        }));
-        p.voice = window.speechSynthesis.getVoices().find(a => a.lang.indexOf(val) == 0) || p.voice;
-        for (let word in p.i18n) p.i18n[word].lang = val
-        return val
-      }
+      (_ = this._).recog.lang = val;
+      _.recog.stop();
+      _.syntaxes = new Map([..._.syntaxes].map(([command, syntax]) => {
+        command.lang = val;
+        syntax.interpretation = command.text.constructor.name === "RegExp" ? command.text : know(command.text);
+        return [command, syntax]
+      }));
+      _.voice = window.speechSynthesis.getVoices().find(a => a.lang.indexOf(val) == 0) || _.voice;
+      return val
     },
-    get active () { return p.active }, set active (_) { return false },
+    get active () { return this._.active }, set active (v) { return false },
 
     // Listen for utterances
     // - Use a named function at .learn() to remove with .forget()
     learn: function (command, fn) {
       var interpretation = command.text.constructor.name === "RegExp" ? command.text : know(command.text);
-      var syntax = p.syntaxes.get(command) || {interpretation, fns: []};
-      p.syntaxes.set(command, syntax);
+      var syntax = (_ = this._).syntaxes.get(command) || {interpretation, fns: []};
+      _.syntaxes.set(command, syntax);
       syntax.fns.push(fn);
-      p.debug && console.log("Learned: " + command.text + (fn.name ? " , listener: " + fn.name : ""))
+      _.debug && console.log("Learned: " + command.text + (fn.name ? " , listener: " + fn.name : ""))
     },
     hear: function (utter) {
       if (utter.constructor.name == "I18nWord") utter = utter.text;
       hear.call(this, Array.isArray(utter) ? utter : [utter])
     },
     forget: function (command, fname = "") {
-      var syntax = p.syntaxes.get(command);
+      var syntax = this._.syntaxes.get(command);
       syntax.fns.splice(syntax.fns.findIndex(fn => fn.name == fname), 1);
-      p.debug && console.log("Unlearned: " + command.text + (fname ? " , listener: " + fname : ""))
+      this._.debug && console.log("Unlearned: " + command.text + (fname ? " , listener: " + fname : ""))
     },
 
     // TODO: NOT CORRECT voiceschanged and voiceReady are racing
@@ -211,13 +192,7 @@ var Speakie = (function () {
     constructor: Speakie
   };
 
-  // Create private properties
-  var p = {}, priv = new WeakMap();
-  return new Proxy(Speakie, { construct (con, args) {
-    var obj = new con(...args); priv.set(obj, p);
-    return new Proxy(obj, { get (obj, key) {
-      p = priv.get(obj); return obj[key]
-    }})
-  }})
+  var _;
+  return Speakie
 
 })();
